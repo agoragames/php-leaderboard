168fe0ade76305fa92ffb532d5e8e9aa690c63bc
David Czarnecki <czarneckid@acm.org>
2011-06-18 16:16:45 -0400

    Initial commit

---
+++ lib/leaderboard/Leaderboard.php
@@ -0,0 +1,35 @@
+<?php
+
+class Leaderboard {
+	const VERSION = '1.0.0';
+	const DEFAULT_PAGE_SIZE = 25;
+	const DEFAULT_HOST = 'localhost';
+	const DEFAULT_PORT = 6379;
+	
+	private $_leaderboard_name;
+	private $_redis_connection;
+	
+	public function __construct($name) {
+		$this->_leaderboard_name = $name;
+		$this->_redis_connection = new Redis();
+		$this->_redis_connection->connect(Leaderboard::DEFAULT_HOST, Leaderboard::DEFAULT_PORT);
+	}
+	
+	public function getLeaderboardName() {
+		return $this->_leaderboard_name;
+	}
+
+	public function close() {
+		return $this->_redis_connection->close();
+	}
+	
+	public function addMember($score, $member) {
+		return $this->_redis_connection->zAdd($this->_leaderboard_name, $score, $member);
+	}
+	
+	public function removeMember($member) {
+		return $this->_redis_connection->zRem($this->_leaderboard_name, $member);
+	}
+}
+
+?>
---
+++ test/LeaderboardTest.php
@@ -0,0 +1,55 @@
+<?php
+
+require '/Users/dczarnecki/projects/php-leaderboard/lib/leaderboard/Leaderboard.php';
+
+class LeaderboardTestSuite extends PHPUnit_Framework_TestCase {
+    public $redis;
+
+	protected function setUp() {
+		$this->redis = new Redis();
+		$this->redis->connect('127.0.0.1', 6379);
+		$this->redis->flushDB();
+	}
+
+    protected function tearDown() { 
+		$this->redis->close();
+    }
+
+	function testAbleToEstablishConnectionToRedis() {
+		$this->assertEquals('+PONG', $this->redis->ping());
+	}
+	
+	function testAbleToSetAKeyToAValue() {
+		$this->redis->set('key', 'value');
+		$this->assertEquals('value', $this->redis->get('key'));
+	}
+	
+	function testVersion() {
+		$this->assertEquals('1.0.0', Leaderboard::VERSION);
+	}
+	
+	function testConstructLeaderboardClassWithName() {
+		$leaderboard = new Leaderboard('leaderboard');
+		$this->assertEquals('leaderboard', $leaderboard->getLeaderboardName());
+	}
+	
+	function testCloseLeaderboardConnection() {
+		$leaderboard = new Leaderboard('leaderboard');
+		$this->assertTrue($leaderboard->close());
+	}
+	
+	function testAddMemberToLeaderboard() {
+		$leaderboard = new Leaderboard('leaderboard');
+		$this->assertEquals(1, $leaderboard->addMember(69, 'david'));
+		$this->assertEquals(1, $this->redis->zSize('leaderboard'));
+	}
+
+	function testRemoveMemberFromLeaderboard() {
+		$leaderboard = new Leaderboard('leaderboard');
+		$this->assertEquals(1, $leaderboard->addMember(69, 'david'));
+		$this->assertEquals(1, $leaderboard->removeMember('david'));
+		$this->assertEquals(0, $this->redis->zSize('leaderboard'));
+	}
+}
+
+?>
