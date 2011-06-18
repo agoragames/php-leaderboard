90988e759534e06447ce20e09e3e7c7124caaeec
David Czarnecki <czarneckid@acm.org>
2011-06-18 17:58:49 -0400

    tabs to spaces

--- lib/leaderboard/Leaderboard.php
+++ lib/leaderboard/Leaderboard.php
@@ -1,121 +1,121 @@
 <?php
 
 class Leaderboard {
-	const VERSION = '1.0.0';
-	const DEFAULT_PAGE_SIZE = 25;
-	const DEFAULT_HOST = 'localhost';
-	const DEFAULT_PORT = 6379;
-	
-	private $_leaderboard_name;
-	private $_redis_connection;
-	
-	public function __construct($name) {
-		$this->_leaderboard_name = $name;
-		$this->_redis_connection = new Redis();
-		$this->_redis_connection->connect(Leaderboard::DEFAULT_HOST, Leaderboard::DEFAULT_PORT);
-	}
-	
-	public function getLeaderboardName() {
-		return $this->_leaderboard_name;
-	}
+    const VERSION = '1.0.0';
+    const DEFAULT_PAGE_SIZE = 25;
+    const DEFAULT_HOST = 'localhost';
+    const DEFAULT_PORT = 6379;
+    
+    private $_leaderboard_name;
+    private $_redis_connection;
+    
+    public function __construct($name) {
+        $this->_leaderboard_name = $name;
+        $this->_redis_connection = new Redis();
+        $this->_redis_connection->connect(Leaderboard::DEFAULT_HOST, Leaderboard::DEFAULT_PORT);
+    }
+    
+    public function getLeaderboardName() {
+        return $this->_leaderboard_name;
+    }
 
-	public function close() {
-		return $this->_redis_connection->close();
-	}
-	
-	public function addMember($score, $member) {
-		return $this->_redis_connection->zAdd($this->_leaderboard_name, $score, $member);
-	}
-	
-	public function removeMember($member) {
-		return $this->_redis_connection->zRem($this->_leaderboard_name, $member);
-	}
-	
-	public function totalMembers() {
-		return $this->_redis_connection->zCard($this->_leaderboard_name);
-	}
-	
-	public function totalPages() {
-		return ceil($this->totalMembers() / Leaderboard::DEFAULT_PAGE_SIZE);
-	}
-	
-	public function rankFor($member, $useZeroIndexForRank = false) {
-	 	$rank = $this->_redis_connection->zRevRank($this->_leaderboard_name, $member);
-		if ($useZeroIndexForRank == false) {
-			$rank += 1;
-		}
-		
-		return $rank;
-	}
-	
-	public function scoreFor($member) {
-		return $this->_redis_connection->zScore($this->_leaderboard_name, $member);
-	}
-	
-	public function leaders($currentPage, $withScores = true, $withRank = true, $useZeroIndexForRank = false) {
-		if ($currentPage < 1) {
-			$currentPage = 1;
-		}
-		
-		if ($currentPage > $this->totalPages()) {
-			$currentPage = $this->totalPages();
-		}
-		
-		$indexForRedis = $currentPage - 1;
-		
-		$startingOffset = ($indexForRedis * Leaderboard::DEFAULT_PAGE_SIZE);
-		if ($startingOffset < 0) {
-			$startingOffset = 0;
-		}
-		
-		$endingOffset = ($startingOffset + Leaderboard::DEFAULT_PAGE_SIZE) - 1;
-		
-		$leaderData = $this->_redis_connection->zRevRange($this->_leaderboard_name, $startingOffset, $endingOffset, $withScores);
-		if (!is_null($leaderData)) {
-			return $this->massageLeaderData($leaderData, $withScores, $withRank, $useZeroIndexForRank);
-		} else {
-			return NULL;
-		}
-	}
-	
-	public function aroundMe($member, $withScores = true, $withRank = true, $useZeroIndexForRank = false) {
-		$reverseRankForMember = $this->_redis_connection->zRevRank($this->_leaderboard_name, $member);
-		
-		$startingOffset = $reverseRankForMember - (Leaderboard::DEFAULT_PAGE_SIZE / 2);
-		if ($startingOffset < 0) {
-			$startingOffset = 0;
-		}
-		
-		$endingOffset = ($startingOffset + Leaderboard::DEFAULT_PAGE_SIZE) - 1;
-		
-		$leaderData = $this->_redis_connection->zRevRange($this->_leaderboard_name, $startingOffset, $endingOffset, $withScores);
-		if (!is_null($leaderData)) {
-			return $this->massageLeaderData($leaderData, $withScores, $withRank, $useZeroIndexForRank);
-		} else {
-			return NULL;
-		}
-	}
-		
-	private function massageLeaderData($leaders, $withScores, $withRank, $useZeroIndexForRank) {
-		$memberAttribute = true;
-		$leaderData = array();
-		
-		$memberData = array();
-		foreach ($leaders as $key => $value) {
+    public function close() {
+        return $this->_redis_connection->close();
+    }
+    
+    public function addMember($score, $member) {
+        return $this->_redis_connection->zAdd($this->_leaderboard_name, $score, $member);
+    }
+    
+    public function removeMember($member) {
+        return $this->_redis_connection->zRem($this->_leaderboard_name, $member);
+    }
+    
+    public function totalMembers() {
+        return $this->_redis_connection->zCard($this->_leaderboard_name);
+    }
+    
+    public function totalPages() {
+        return ceil($this->totalMembers() / Leaderboard::DEFAULT_PAGE_SIZE);
+    }
+    
+    public function rankFor($member, $useZeroIndexForRank = false) {
+        $rank = $this->_redis_connection->zRevRank($this->_leaderboard_name, $member);
+        if ($useZeroIndexForRank == false) {
+            $rank += 1;
+        }
+        
+        return $rank;
+    }
+    
+    public function scoreFor($member) {
+        return $this->_redis_connection->zScore($this->_leaderboard_name, $member);
+    }
+    
+    public function leaders($currentPage, $withScores = true, $withRank = true, $useZeroIndexForRank = false) {
+        if ($currentPage < 1) {
+            $currentPage = 1;
+        }
+        
+        if ($currentPage > $this->totalPages()) {
+            $currentPage = $this->totalPages();
+        }
+        
+        $indexForRedis = $currentPage - 1;
+        
+        $startingOffset = ($indexForRedis * Leaderboard::DEFAULT_PAGE_SIZE);
+        if ($startingOffset < 0) {
+            $startingOffset = 0;
+        }
+        
+        $endingOffset = ($startingOffset + Leaderboard::DEFAULT_PAGE_SIZE) - 1;
+        
+        $leaderData = $this->_redis_connection->zRevRange($this->_leaderboard_name, $startingOffset, $endingOffset, $withScores);
+        if (!is_null($leaderData)) {
+            return $this->massageLeaderData($leaderData, $withScores, $withRank, $useZeroIndexForRank);
+        } else {
+            return NULL;
+        }
+    }
+    
+    public function aroundMe($member, $withScores = true, $withRank = true, $useZeroIndexForRank = false) {
+        $reverseRankForMember = $this->_redis_connection->zRevRank($this->_leaderboard_name, $member);
+        
+        $startingOffset = $reverseRankForMember - (Leaderboard::DEFAULT_PAGE_SIZE / 2);
+        if ($startingOffset < 0) {
+            $startingOffset = 0;
+        }
+        
+        $endingOffset = ($startingOffset + Leaderboard::DEFAULT_PAGE_SIZE) - 1;
+        
+        $leaderData = $this->_redis_connection->zRevRange($this->_leaderboard_name, $startingOffset, $endingOffset, $withScores);
+        if (!is_null($leaderData)) {
+            return $this->massageLeaderData($leaderData, $withScores, $withRank, $useZeroIndexForRank);
+        } else {
+            return NULL;
+        }
+    }
+        
+    private function massageLeaderData($leaders, $withScores, $withRank, $useZeroIndexForRank) {
+        $memberAttribute = true;
+        $leaderData = array();
+        
+        $memberData = array();
+        foreach ($leaders as $key => $value) {
 
-			$memberData['member'] = $key;
-			$memberData['score'] = $value;
+            $memberData['member'] = $key;
+            $memberData['score'] = $value;
 
-			if ($withRank) {
-				$memberData['rank'] = $this->rankFor($key, $useZeroIndexForRank);
-			}
+            if ($withRank) {
+                $memberData['rank'] = $this->rankFor($key, $useZeroIndexForRank);
+            }
 
-			array_push($leaderData, $memberData);
-			$memberData = array();
-		}
-		
-		return $leaderData;
-	}	
+            array_push($leaderData, $memberData);
+            $memberData = array();
+        }
+        
+        return $leaderData;
+    }   
 }
 
 ?>
--- test/LeaderboardTest.php
+++ test/LeaderboardTest.php
@@ -5,114 +5,114 @@ require '/Users/dczarnecki/projects/php-leaderboard/lib/leaderboard/Leaderboard.
 class LeaderboardTestSuite extends PHPUnit_Framework_TestCase {
     public $redis;
 
-	protected function setUp() {
-		$this->redis = new Redis();
-		$this->redis->connect('127.0.0.1', 6379);
-		$this->redis->flushDB();
-	}
+    protected function setUp() {
+        $this->redis = new Redis();
+        $this->redis->connect('127.0.0.1', 6379);
+        $this->redis->flushDB();
+    }
 
     protected function tearDown() { 
-		$this->redis->close();
+        $this->redis->close();
     }
-	
-	function testVersion() {
-		$this->assertEquals('1.0.0', Leaderboard::VERSION);
-	}
-	
-	function testConstructLeaderboardClassWithName() {
-		$leaderboard = new Leaderboard('leaderboard');
-		$this->assertEquals('leaderboard', $leaderboard->getLeaderboardName());
-	}
-	
-	function testCloseLeaderboardConnection() {
-		$leaderboard = new Leaderboard('leaderboard');
-		$this->assertTrue($leaderboard->close());
-	}
-	
-	function testAddMember() {
-		$leaderboard = new Leaderboard('leaderboard');
-		$this->assertEquals(1, $leaderboard->addMember(69, 'david'));
-		$this->assertEquals(1, $this->redis->zSize('leaderboard'));
-	}
+    
+    function testVersion() {
+        $this->assertEquals('1.0.0', Leaderboard::VERSION);
+    }
+    
+    function testConstructLeaderboardClassWithName() {
+        $leaderboard = new Leaderboard('leaderboard');
+        $this->assertEquals('leaderboard', $leaderboard->getLeaderboardName());
+    }
+    
+    function testCloseLeaderboardConnection() {
+        $leaderboard = new Leaderboard('leaderboard');
+        $this->assertTrue($leaderboard->close());
+    }
+    
+    function testAddMember() {
+        $leaderboard = new Leaderboard('leaderboard');
+        $this->assertEquals(1, $leaderboard->addMember(69, 'david'));
+        $this->assertEquals(1, $this->redis->zSize('leaderboard'));
+    }
 
-	function testRemoveMember() {
-		$leaderboard = new Leaderboard('leaderboard');
-		$this->assertEquals(1, $leaderboard->addMember(69, 'david'));
-		$this->assertEquals(1, $leaderboard->removeMember('david'));
-		$this->assertEquals(0, $this->redis->zSize('leaderboard'));
-	}
+    function testRemoveMember() {
+        $leaderboard = new Leaderboard('leaderboard');
+        $this->assertEquals(1, $leaderboard->addMember(69, 'david'));
+        $this->assertEquals(1, $leaderboard->removeMember('david'));
+        $this->assertEquals(0, $this->redis->zSize('leaderboard'));
+    }
 
-	function testTotalMembers() {
-		$leaderboard = new Leaderboard('leaderboard');
-		$this->assertEquals(1, $leaderboard->addMember(69, 'david'));
-		$this->assertEquals(1, $leaderboard->totalMembers());
-	}
-	
-	function testTotalPages() {
-		$leaderboard = new Leaderboard('leaderboard');
-		for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE + 1; $i++) {
-		    $leaderboard->addMember($i, "member_{$i}");
-		}
-		
-		$this->assertEquals(2, $leaderboard->totalPages());
-	}
-	
-	function testRankFor() {
-		$leaderboard = new Leaderboard('leaderboard');
-		for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE + 1; $i++) {
-		    $leaderboard->addMember($i, "member_{$i}");
-		}
-		
-		$this->assertEquals(26, $leaderboard->rankFor('member_1'));
-		$this->assertEquals(25, $leaderboard->rankFor('member_1', true));
-	}
+    function testTotalMembers() {
+        $leaderboard = new Leaderboard('leaderboard');
+        $this->assertEquals(1, $leaderboard->addMember(69, 'david'));
+        $this->assertEquals(1, $leaderboard->totalMembers());
+    }
+    
+    function testTotalPages() {
+        $leaderboard = new Leaderboard('leaderboard');
+        for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE + 1; $i++) {
+            $leaderboard->addMember($i, "member_{$i}");
+        }
+        
+        $this->assertEquals(2, $leaderboard->totalPages());
+    }
+    
+    function testRankFor() {
+        $leaderboard = new Leaderboard('leaderboard');
+        for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE + 1; $i++) {
+            $leaderboard->addMember($i, "member_{$i}");
+        }
+        
+        $this->assertEquals(26, $leaderboard->rankFor('member_1'));
+        $this->assertEquals(25, $leaderboard->rankFor('member_1', true));
+    }
 
-	function testScoreFor() {
-		$leaderboard = new Leaderboard('leaderboard');
-		for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE + 1; $i++) {
-		    $leaderboard->addMember($i, "member_{$i}");
-		}
-		
-		$this->assertEquals(14, $leaderboard->scoreFor('member_14'));
-	}
-	
-	function testLeaders() {
-		$leaderboard = new Leaderboard('leaderboard');
-		for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE + 1; $i++) {
-		    $leaderboard->addMember($i, "member_{$i}");
-		}
+    function testScoreFor() {
+        $leaderboard = new Leaderboard('leaderboard');
+        for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE + 1; $i++) {
+            $leaderboard->addMember($i, "member_{$i}");
+        }
+        
+        $this->assertEquals(14, $leaderboard->scoreFor('member_14'));
+    }
+    
+    function testLeaders() {
+        $leaderboard = new Leaderboard('leaderboard');
+        for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE + 1; $i++) {
+            $leaderboard->addMember($i, "member_{$i}");
+        }
 
-		$leaders = $leaderboard->leaders(1);
-		$this->assertEquals(Leaderboard::DEFAULT_PAGE_SIZE, count($leaders));
-		$this->assertEquals('member_26', $leaders[0]['member']);
-		$this->assertEquals(26, $leaders[0]['score']);
-		$this->assertEquals(1, $leaders[0]['rank']);
-		
-		$leaders = $leaderboard->leaders(2);
-		$this->assertEquals(1, count($leaders));
-		$this->assertEquals('member_1', $leaders[0]['member']);
-		$this->assertEquals(1, $leaders[0]['score']);
-		$this->assertEquals(26, $leaders[0]['rank']);
-	}
-	
-	function testAroundMe() {
-		$leaderboard = new Leaderboard('leaderboard');
-	
-		for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE * 3 + 1; $i++) {
-		    $leaderboard->addMember($i, "member_{$i}");
-		}
-		
-		$this->assertEquals(Leaderboard::DEFAULT_PAGE_SIZE * 3 + 1, $leaderboard->totalMembers());
-		
-		$leadersAroundMe = $leaderboard->aroundMe('member_30');
-		$this->assertEquals(Leaderboard::DEFAULT_PAGE_SIZE / 2, count($leadersAroundMe) / 2);
+        $leaders = $leaderboard->leaders(1);
+        $this->assertEquals(Leaderboard::DEFAULT_PAGE_SIZE, count($leaders));
+        $this->assertEquals('member_26', $leaders[0]['member']);
+        $this->assertEquals(26, $leaders[0]['score']);
+        $this->assertEquals(1, $leaders[0]['rank']);
+        
+        $leaders = $leaderboard->leaders(2);
+        $this->assertEquals(1, count($leaders));
+        $this->assertEquals('member_1', $leaders[0]['member']);
+        $this->assertEquals(1, $leaders[0]['score']);
+        $this->assertEquals(26, $leaders[0]['rank']);
+    }
+    
+    function testAroundMe() {
+        $leaderboard = new Leaderboard('leaderboard');
+    
+        for ($i = 1; $i <= Leaderboard::DEFAULT_PAGE_SIZE * 3 + 1; $i++) {
+            $leaderboard->addMember($i, "member_{$i}");
+        }
+        
+        $this->assertEquals(Leaderboard::DEFAULT_PAGE_SIZE * 3 + 1, $leaderboard->totalMembers());
+        
+        $leadersAroundMe = $leaderboard->aroundMe('member_30');
+        $this->assertEquals(Leaderboard::DEFAULT_PAGE_SIZE / 2, count($leadersAroundMe) / 2);
 
-		$leadersAroundMe = $leaderboard->aroundMe('member_1');
-		$this->assertEquals(ceil(Leaderboard::DEFAULT_PAGE_SIZE / 2 + 1), count($leadersAroundMe));
+        $leadersAroundMe = $leaderboard->aroundMe('member_1');
+        $this->assertEquals(ceil(Leaderboard::DEFAULT_PAGE_SIZE / 2 + 1), count($leadersAroundMe));
 
-		$leadersAroundMe = $leaderboard->aroundMe('member_76');
-		$this->assertEquals(Leaderboard::DEFAULT_PAGE_SIZE / 2, count($leadersAroundMe) / 2);
-	}
+        $leadersAroundMe = $leaderboard->aroundMe('member_76');
+        $this->assertEquals(Leaderboard::DEFAULT_PAGE_SIZE / 2, count($leadersAroundMe) / 2);
+    }
 }
 
 ?>
